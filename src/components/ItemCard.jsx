1a70acf5b6004d640019161565bb9fb4762fd8ee
Rodrigo Soberon <rodrigo.soberon.a@gmail.com>
2022-07-09 12:41:30 -0300

    precios con descuento

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,21 +1,47 @@
 import { Card, Button, Badge } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-function ItemCard({ title, description, price, image, id, discount, available }) {
+function ItemCard({
+  title,
+  description,
+  price,
+  image,
+  id,
+  discount,
+  available,
+}) {
+
+  const discountPrice = Math.round(price * (100 - discount) / 100);
+
   return (
     <Card style={{ width: "18rem" }} className="m-2 p-0">
       <Card.Img variant="top" src={image} />
       <Card.Body>
         <Card.Title>
           {title}
-          {discount && <Badge className="ms-1" bg="info">{discount}% off!</Badge>}
-          {available == 0 && <Badge className="ms-1" bg="danger">Sin stock</Badge>}
+          {discount && (
+            <Badge className="ms-1" bg="info">
+              {discount}% off!
+            </Badge>
+          )}
+          {available == 0 && (
+            <Badge className="ms-1" bg="danger">
+              Sin stock
+            </Badge>
+          )}
         </Card.Title>
         <Card.Text>{description}</Card.Text>
       </Card.Body>
-      <Card.Footer className="bg-secondary text-light">
-        <div className="text-center fs-3">{`$ ${price}`}</div>
-      </Card.Footer>
+      {discount ? (
+        <Card.Footer className="bg-info d-flex flex-row justify-content-center align-items-center gap-2">
+          <div className="text-center fs-3 text-light ">{`$ ${discountPrice}`}</div>
+          <div className="text-center  text-black-50 text-decoration-line-through">{`$ ${price}`}</div>
+        </Card.Footer>
+      ) : (
+        <Card.Footer className="bg-secondary text-light">
+          <div className="text-center fs-3">{`$ ${price}`}</div>
+        </Card.Footer>
+      )}
       <Link to={`/item/${id}`} className="text-center">
         <Button variant="dark" className="m-3">
           Ver detalle
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -17,15 +17,29 @@ function ItemDetail({ item }) {
     addItem(itemToCart);
   };
 
+  const discountPrice = Math.round(item.price * (100 - item.discount) / 100);
+
   return (
     <Row>
       <h2>
         {item.title}
-        {item.discount && <Badge className="ms-1" bg="info">{item.discount}% off!</Badge>}
-        </h2>
+        {item.discount && (
+          <Badge className="ms-1" bg="info">
+            {item.discount}% off!
+          </Badge>
+        )}
+      </h2>
       <img src={item.image} alt={item.title} />
       <p className="py-2 fs-5">{item.description}</p>
-      <h3 className="fs-2 text-center bg-secondary text-light py-2">{`$ ${item.price}`}</h3>
+      {item.discount ? (
+        <div className="bg-info d-flex flex-row justify-content-center align-items-center gap-2 py-2">
+          <div className="fs-2 text-light ">{`$ ${discountPrice}`}</div>
+          <div className="text-black-50 text-decoration-line-through">{`$ ${item.price}`}</div>
+        </div>
+      ) : (
+        <h3 className="fs-2 text-center bg-secondary text-light py-2">{`$ ${item.price}`}</h3>
+      )}
+
       <Container fluid className="my-3">
         {isInCart(item.id) ? (
           <Alert variant="success">
